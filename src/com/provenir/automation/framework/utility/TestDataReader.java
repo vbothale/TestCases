c86f9b312ce3eceb47c233d522f2cd0012a214b3
Parashar Bajpai <parasharb@cybage.com>
2015-09-16 19:28:39 +0530

    added method to update JSON file through Hashmap

--- src/com/provenir/automation/framework/utility/TestDataReader.java
+++ src/com/provenir/automation/framework/utility/TestDataReader.java
@@ -1,25 +1,28 @@
 package com.provenir.automation.framework.utility;
 
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 import com.provenir.automation.framework.utility.TestDataReader;
 
 public class TestDataReader {
-	
+
 	private final static Logger log = Logger.getLogger(TestDataReader.class);
-	
+
 	// reading value from JSON
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void readValue(
-			HashMap<String, Map<String, String>> hashMap, String str) {
+	public void readValue(HashMap<String, Map<String, String>> hashMap,
+			String str) {
 		try {
 
 			System.out.println(str);
@@ -44,24 +47,24 @@ public class TestDataReader {
 				hashMap.put(key, properties);
 			}
 		} catch (Exception e) {
-//			e.printStackTrace();
+			// e.printStackTrace();
 			log.error(e.getLocalizedMessage());
 		}
-	
+
 	}
-	
+
 	// updating value into JSON from Hashmap
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void updateValue(
-			HashMap<String, Map<String, String>> hashMap, String str, String value) {
+			HashMap<String, Map<String, String>> hashMap, String str,
+			String value, String value1) {
 		try {
 			System.out.println(str);
 			JSONObject jsonObject = (JSONObject) new JSONParser()
 					.parse(new FileReader(str));
 			Iterator iter = jsonObject.entrySet().iterator();
-			while(iter.hasNext())
-			{
+			while (iter.hasNext()) {
 				Map.Entry<String, Map<String, String>> entryMap = (Entry<String, Map<String, String>>) iter
 						.next();
 				HashMap<String, String> properties = new HashMap<String, String>();
@@ -73,23 +76,74 @@ public class TestDataReader {
 					Map.Entry<String, String> propertyEntry = (Entry<String, String>) propIter
 							.next();
 					properties.put(propertyEntry.getKey(),
-							propertyEntry.setValue(value));
+							propertyEntry.getValue());
+
+					propertyEntry.setValue(value1);
+
 				}
 
-				hashMap.put(key, properties); 
+				hashMap.put(key, properties);
 			}
 		} catch (Exception e) {
-//			e.printStackTrace();
+
 			log.error(e.getLocalizedMessage());
 		}
 	}
 	
-	// deleting value from JSON through Hashmap
 	
-	public static void deleteValue(
-			HashMap<String, Map<String, String>> hashMap, String str, String value) {
-		
-	}
 
+	// public static Map<String, Object> jsonToMap(JsonObject json) {
+	// Map<String, Object> retMap = new HashMap<String, Object>();
+	//
+	// if(json != JsonObject.NULL) {
+	// retMap = toMap(json);
+	// }
+	// return retMap;
+	// }
+	//
+	// public static Map<String, Object> toMap(JsonObject object) throws
+	// JsonException {
+	// Map<String, Object> map = new HashMap<String, Object>();
+	//
+	// Iterator<String> keysItr = object.keySet().iterator();
+	// while(keysItr.hasNext()) {
+	// String key = keysItr.next();
+	// Object value = object.get(key);
+	//
+	// if(value instanceof JsonArray) {
+	// value = toList((JsonArray) value);
+	// }
+	//
+	// else if(value instanceof JsonObject) {
+	// value = toMap((JsonObject) value);
+	// }
+	// map.put(key, value);
+	// }
+	// return map;
+	// }
+	//
+	// public static List<Object> toList(JsonArray array) {
+	// List<Object> list = new ArrayList<Object>();
+	// for(int i = 0; i < array.size(); i++) {
+	// Object value = array.get(i);
+	// if(value instanceof JsonArray) {
+	// value = toList((JsonArray) value);
+	// }
+	//
+	// else if(value instanceof JsonObject) {
+	// value = toMap((JsonObject) value);
+	// }
+	// list.add(value);
+	// }
+	// return list;
+	// }
+
+	// deleting value from JSON through Hashmap
+
+	public static void deleteValue(
+			HashMap<String, Map<String, String>> hashMap, String str,
+			String value) {
+
+	}
 
 }
